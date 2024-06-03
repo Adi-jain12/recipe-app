980ee410fcc7b28d1802133414c63f3a9cceb1db
aditya <jaditya98@gmail.com>
2024-06-03 23:51:20 +0530

    add recipe apis endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,42 @@
 import express from "express";
 import cors from "cors";
+import * as RecipeAPI from "./recipe-api";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prismaClient = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/api/recipes/search", async (req, res) => {
-  res.json({ message: "Success!" });
+  const searchTerm = req.query.searchTerm as string;
+  const page = parseInt(req.query.page as string);
+  const results = await RecipeAPI.searchRecipes(searchTerm, page);
+
+  return res.json(results);
+});
+
+app.get("/api/recipes/:recipeId/summary", async (req, res) => {
+  const recipeId = req.params.recipeId;
+  const results = await RecipeAPI.getRecipeSummary(recipeId);
+  return res.json(results);
+});
+
+app.post("/api/recipes/favourite", async (req, res) => {
+  const recipeId = req.body.recipeId;
+
+  try {
+    const favouriteRecipe = await prismaClient.favouriteRecipes.create({
+      data: {
+        recipeId: recipeId,
+      },
+    });
+    return res.status(201).json(favouriteRecipe);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Oops, something went wrong" });
+  }
 });
 
 app.listen(5000, () => {
